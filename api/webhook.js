4edf07259f0c1d8609403e3a20e788c0ff8b83e1
luhandrade11 <andy.dwsmall@googlemail.com>
2026-05-01 19:30:52 -0300

    Add files via upload

--- api/webhook.js
+++ api/webhook.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Always respond 200 immediately so WayMB confirms delivery
   res.status(200).json({ received: true });
 
